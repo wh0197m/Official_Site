5167832d78466c3e3c36392d9c614bde66e72379
peakmonkey <pengsuiwan@langnal.com>
2017-09-21 15:46:35 +0800

    增加百度地图

--- source/js/app.js
+++ source/js/app.js
@@ -194,11 +194,73 @@ function animation() {
     });
 }
 
+function buildMap() {
+    function initMap() {
+        createMap(); //创建地图
+        setMapEvent(); //设置地图事件
+        // addMapControl(); //向地图添加控件
+        addMapOverlay(); //向地图添加覆盖物
+    }
+
+    function createMap() {
+        map = new BMap.Map("map");
+        map.centerAndZoom(new BMap.Point(114.362629, 30.532696), 17);
+    }
+
+    function setMapEvent() {}
+
+    function addClickHandler(target, window) {
+        target.addEventListener("click", function() {
+            target.openInfoWindow(window);
+        });
+    }
+
+    function addMapOverlay() {
+        var markers = [
+            { content: "中部创意大厦写字楼2308", title: "朗信科技", imageOffset: { width: -46, height: -21 }, position: { lat: 30.53279, lng: 114.361856 } }
+        ];
+        for (var index = 0; index < markers.length; index++) {
+            var point = new BMap.Point(markers[index].position.lng, markers[index].position.lat);
+            var marker = new BMap.Marker(point, {
+                icon: new BMap.Icon("http://api.map.baidu.com/lbsapi/createmap/images/icon.png", new BMap.Size(20, 25), {
+                    imageOffset: new BMap.Size(markers[index].imageOffset.width, markers[index].imageOffset.height)
+                })
+            });
+            var label = new BMap.Label(markers[index].title, {
+                offset: new BMap.Size(18, 12)
+            });
+            label.setStyle({
+                border: 'none',
+                'font-size': '12px',
+                'color': '#512da8'
+            });
+            var opts = {
+                width: 200,
+                title: markers[index].title,
+                enableMessage: false
+            };
+            var infoWindow = new BMap.InfoWindow(markers[index].content, opts);
+            marker.setLabel(label);
+            addClickHandler(marker, infoWindow);
+            map.addOverlay(marker);
+        };
+    }
+    //向地图添加控件
+    function addMapControl() {
+        var navControl = new BMap.NavigationControl({ anchor: BMAP_ANCHOR_TOP_LEFT, type: 1 });
+        map.addControl(navControl);
+    }
+    var map;
+    initMap();
+}
+
 $(document).ready(function() {
 
     // 首页则加载粒子动画
     if (window.location.pathname === '/') {
         animation();
+    } else if (window.location.pathname === '/contact.html') {
+        buildMap();
     }
 
     // 在console出现指定内容
